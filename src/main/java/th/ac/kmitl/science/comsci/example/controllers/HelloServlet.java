066d47688f43725c58c13b898a1b7ad778a59477
passapong.t <passapong.t@gmail.com>
2017-10-29 12:22:39 +0700

    created and added doGet

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/controllers/HelloServlet.java
@@ -0,0 +1,23 @@
+package th.ac.kmitl.science.comsci.example.controllers;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet
+        extends HttpServlet {
+
+    public HelloServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String message = "Hello World";
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+}
