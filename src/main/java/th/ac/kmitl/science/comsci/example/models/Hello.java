d4256045ecfdb581a3086901395d0d62e596c9ab
passapong.t <passapong.t@gmail.com>
2017-11-05 17:12:41 +0700

    created and added getMessage

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Hello.java
@@ -0,0 +1,11 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+public class Hello {
+
+    private String message = "Hello World";
+
+    public String getMessage() {
+        return message;
+    }
+
+}
