239e10e273e9fa536716d5f3819195a09155fd9e
nontakho <nontakho@gmail.com>
2017-11-10 23:14:38 +0700

    Insert 570502522 Nontakhon kerdmongkol to servlet

--- src/main/java/th/ac/kmitl/science/comsci/example/controllers/HelloServlet.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/controllers/HelloServlet.java
@@ -27,7 +27,7 @@ public class HelloServlet
         out.println("<li>58050276 Thanathat Surakhup</li>");
         out.println("<li>58050309 Pasthiyakan Kaewpitak</li>");
         out.println("<li>58050355 Panuwich Laoudom</li>");
-	out.println("<li>57050252 Nontakhon kerdmongkol</li>");
+        out.println("<li>57050252 Nontakhon kerdmongkol</li>");
         out.println("</ul>");
     }
 }
