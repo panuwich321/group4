2a03fb35bb1da1f49f7646183eae21e2b648dcca
Chayapol Limanon <chayapol.limanon@gmail.com>
2017-11-13 02:00:38 +0700

    Add a missing name (57050203 Charunthron Rattanamanoporn)

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/nameprinters/CharunthronNamePrinter.java
@@ -0,0 +1,9 @@
+package th.ac.kmitl.science.comsci.example.nameprinters;
+
+public class CharunthronNamePrinter extends NamePrinterBase {
+
+    @Override
+    public String getName() {
+        return "57050203 Charunthron Rattanamanoporn";
+    }
+}
