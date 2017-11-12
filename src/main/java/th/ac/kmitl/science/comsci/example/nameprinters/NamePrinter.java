61ea50b6f9873e5d27f357e2f954e8f5614a7747
Chayapol Limanon <chayapol.limanon@gmail.com>
2017-11-12 17:01:44 +0700

    Add nameprinters package:

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/nameprinters/NamePrinter.java
@@ -0,0 +1,9 @@
+package th.ac.kmitl.science.comsci.example.nameprinters;
+
+import java.io.PrintWriter;
+
+public interface NamePrinter
+{
+    String getName();
+    void print(PrintWriter writer);
+}
