52a3dcf52abbe36ea9e3f84dc843986ad72c88d9
Dear <dear11159@hotmail.com>
2017-11-15 22:13:43 +0700

    Add Nunthinee to nameprinters

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/nameprinters/NunthineeNamePrinter.java
@@ -0,0 +1,15 @@
+package th.ac.kmitl.science.comsci.example.nameprinters;
+
+public class NontakonNamePrinter extends BaseNameWithIdPrinter {
+
+    @Override
+    public String getName() {
+        return "Nunthinee Thongprom";
+    }
+
+
+    @Override
+    public String getId() {
+        return "58050307";
+    }
+}
