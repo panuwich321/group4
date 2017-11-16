3518d3eb8434d20a40060cfaa923700c939b2850
58050231 <58050231@kmitl.ac.th>
2017-11-17 05:57:57 +0700

    Add PattharachatNamePrinter.java

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/nameprinters/PattharachatNamePrinter.java
@@ -0,0 +1,14 @@
+package th.ac.kmitl.science.comsci.example.nameprinters;
+
+public class PattharachatNamePrinter extends BaseNameWithIdPrinter {
+
+    @Override
+    public String getName() {
+        return "Pattharachat Chanakitkumjorn";
+    }
+
+    @Override
+    public String getId() {
+        return "58050231";
+    }
+}
