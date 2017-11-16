3f5cdb7c98dd67e3a46dd229c5ccce6add62261b
Auttapon <iceey7@gmail.com>
2017-11-16 11:37:10 +0700

    add

--- src/main/java/th/ac/kmitl/science/comsci/example/controllers/HelloServlet.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/controllers/HelloServlet.java
@@ -1,10 +1,14 @@
 package th.ac.kmitl.science.comsci.example.controllers;
 
 import th.ac.kmitl.science.comsci.example.models.Hello;
+import th.ac.kmitl.science.comsci.example.nameprinters.NamePrinterLoader;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet
         extends HttpServlet {
@@ -20,9 +24,10 @@ public class HelloServlet
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<h1>" + message + "</h1>");
-	out.println("<ul>")
-	out.println("<li>Auttapon</li>");
-	out.println("</ul>")
+        out.println("<ul>");
+        
+        NamePrinterLoader.getInstance().printAllNames(out);
+        
+        out.println("</ul>");
     }
-
 }
