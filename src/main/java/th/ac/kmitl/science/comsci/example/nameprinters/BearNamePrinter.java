2f553c2d8f19a00eafc223c6374f84aa81070d64
passapong.t <passapong.t@gmail.com>
2017-11-18 12:21:41 +0700

    removed

--- src/main/java/th/ac/kmitl/science/comsci/example/nameprinters/BearNamePrinter.java
+++
@@ -1,9 +0,0 @@
-package th.ac.kmitl.science.comsci.example.nameprinters;
-
-public class BearNamePrinter extends BaseNamePrinter {
-
-    @Override
-    public String getName() {
-        return "Bearchitect";
-    }
-}
