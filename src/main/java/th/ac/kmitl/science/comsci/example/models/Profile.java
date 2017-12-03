1b862dad55916bd98d2317c8443a45c37bb28722
Auttapon <iceey7@gmail.com>
2017-12-03 15:22:19 +0700

    add Buyer Model

--- src/main/java/th/ac/kmitl/science/comsci/example/models/Profile.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Profile.java
@@ -1,8 +1,12 @@
-
 package th.ac.kmitl.science.comsci.example.models;
 
 public abstract class Profile {
+
     protected String id;
+
     protected String globalId;
-    protected String name;  
+
+    protected String name;
+    
+    protected String taxId;  
 }
