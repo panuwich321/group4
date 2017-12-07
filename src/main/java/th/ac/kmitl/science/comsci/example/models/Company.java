e2b3dab3122180c0d2c1b10e2d498928173263b6
Auttapon <iceey7@gmail.com>
2017-12-07 15:33:24 +0700

    fix indent

--- src/main/java/th/ac/kmitl/science/comsci/example/models/Company.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Company.java
@@ -7,36 +7,36 @@ public class Company{
 	String name;
 	String taxId;
 	
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getGlobalId() {
-        return globalId;
-    }
+	public String getGlobalId() {
+		return globalId;
+	}
 
-    public void setGlobalId(String globalId) {
-        this.globalId = globalId;
-    }
+	public void setGlobalId(String globalId) {
+		this.globalId = globalId;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
     
-    public void setTaxId(String taxId) {
-        this.taxId = taxId;
-    }
+	public void setTaxId(String taxId) {
+		this.taxId = taxId;
+	}
     
-    public String getTaxId() {
-        return taxId;
-    }
+	public String getTaxId() {
+		return taxId;
+	}
 
 }
