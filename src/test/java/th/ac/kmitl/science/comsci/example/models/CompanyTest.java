a7bced73e39a6c5cfd6684796a41da993009bb50
Auttapon <iceey7@gmail.com>
2017-12-07 15:40:34 +0700

    fix indent

--- src/test/java/th/ac/kmitl/science/comsci/example/models/CompanyTest.java
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/CompanyTest.java
@@ -6,31 +6,31 @@ public class CompanyTest {
 
 	Company Testinputdata = new Company();
 	
-    @Test
-    public void testgetId() {
-    	String id = "id58050418";
-       	Testinputdata.setId(id);
-       	assertEquals(Testinputdata.getId(), "id58050418");
+	@Test
+	public void testgetId() {
+		String id = "id58050418";
+		Testinputdata.setId(id);
+		assertEquals(Testinputdata.getId(), "id58050418");
     }
     
-    @Test
-    public void testgetName() {
-    	String name = "Auttapon Kan-uphai";
-    	Testinputdata.setName(name);
-    	assertEquals(Testinputdata.getName(), "Auttapon Kan-uphai");
+	@Test
+	public void testgetName() {
+		String name = "Auttapon Kan-uphai";
+		Testinputdata.setName(name);
+		assertEquals(Testinputdata.getName(), "Auttapon Kan-uphai");
     }
     
-    @Test
-    public void testgetGlobalId() {
-    	String globalId = "GId58050418";
-    	Testinputdata.setGlobalId(globalId);
-    	assertEquals(Testinputdata.getGlobalId(), "GId58050418");
-    }
+	@Test
+	public void testgetGlobalId() {
+		String globalId = "GId58050418";
+		Testinputdata.setGlobalId(globalId);
+		assertEquals(Testinputdata.getGlobalId(), "GId58050418");
+	}
     
-    @Test
-    public void testgetTaxId() {
-    	String taxId = "TId58050418";
-        Testinputdata.setTaxId(taxId);
-        assertEquals(Testinputdata.getTaxId(), "TId58050418");
-    }
+	@Test
+	public void testgetTaxId() {
+		String taxId = "TId58050418";
+		Testinputdata.setTaxId(taxId);
+		assertEquals(Testinputdata.getTaxId(), "TId58050418");
+	}
 }
