35491ec52017812c41be996d65adf0271a397672
Auttapon <iceey7@gmail.com>
2017-12-07 16:30:51 +0700

    rename Testinputdata to company

--- src/test/java/th/ac/kmitl/science/comsci/example/models/CompanyTest.java
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/CompanyTest.java
@@ -4,33 +4,33 @@ import static org.junit.Assert.assertEquals;
 
 public class CompanyTest {
 
-	Company Testinputdata = new Company();
+	Company company = new Company();
 	
 	@Test
 	public void testgetId() {
 		String id = "id58050418";
-		Testinputdata.setId(id);
-		assertEquals(Testinputdata.getId(), "id58050418");
+		company.setId(id);
+		assertEquals(company.getId(), "id58050418");
     }
     
 	@Test
 	public void testgetName() {
 		String name = "Auttapon Kan-uphai";
-		Testinputdata.setName(name);
-		assertEquals(Testinputdata.getName(), "Auttapon Kan-uphai");
+		company.setName(name);
+		assertEquals(company.getName(), "Auttapon Kan-uphai");
     }
     
 	@Test
 	public void testgetGlobalId() {
 		String globalId = "GId58050418";
-		Testinputdata.setGlobalId(globalId);
-		assertEquals(Testinputdata.getGlobalId(), "GId58050418");
+		company.setGlobalId(globalId);
+		assertEquals(company.getGlobalId(), "GId58050418");
 	}
     
 	@Test
 	public void testgetTaxId() {
 		String taxId = "TId58050418";
-		Testinputdata.setTaxId(taxId);
-		assertEquals(Testinputdata.getTaxId(), "TId58050418");
+		company.setTaxId(taxId);
+		assertEquals(company.getTaxId(), "TId58050418");
 	}
 }
